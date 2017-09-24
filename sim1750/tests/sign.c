d1fbdac168ad4b82c23368896d0094c122b028da
Oliver Kellogg <okellogg@users.sourceforge.net>
2017-09-24 22:11:43 +0200

    sim1750/tests/sign.c sim1750/tests/uadd.c sim1750/tests/usub.c - Avoid type `long', its size varies among host architectures.

--- sim1750/tests/sign.c
+++ sim1750/tests/sign.c
@@ -2,11 +2,11 @@
 
 main (int argc, char *argv[])
 {
-  long n;
+  int n;
 
-  sscanf (argv[1], "%li", &n);
+  sscanf (argv[1], "%i", &n);
   if (!strncmp (argv[1], "0x", 2) && strlen (argv[1]) == 6 && (n & 0x8000L))
     n |= 0xFFFF0000L;
-  printf ("0x%08lX => %ld\n", n, n);
+  printf ("0x%08X => %d\n", n, n);
 }
 
--- sim1750/tests/usub.c
+++ sim1750/tests/usub.c
@@ -3,13 +3,13 @@
 main (int argc, char *argv[])
 {
   short n1, n2;
-  unsigned long op1, op2, result;
+  unsigned op1, op2, result;
 
   sscanf (argv[1], "%hd", &n1);
   sscanf (argv[2], "%hd", &n2);
   op1 = *(unsigned short *) &n1;
   op2 = *(unsigned short *) &n2;
   result = op1 - op2;
-  printf ("%08lX - %08lX = %08lX\n", op1, op2, result);
+  printf ("%08X - %08X = %08X\n", op1, op2, result);
 }
 
